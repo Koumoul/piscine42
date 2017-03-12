acd3c3a1c802469713cd7bfd5fa75075c9f475f9
Nicolas BOURHIS <nbourhis@e3r11p4.42.fr>
2017-03-12 20:45:36 +0100

    Day04

---
+++ Day04/ft_iterative_factorial.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbourhis <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/12 20:17:04 by nbourhis          #+#    #+#             */
+/*   Updated: 2017/03/12 20:44:27 by nbourhis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int     ft_iterative_factorial(int nb)
+{
+	int i;
+	int fact;
+
+	i = 2;
+	fact = 1;
+	while (i <= nb)
+	{
+		fact = fact * i;
+		i++;
+	}
+	return (fact);
+}
+
+int  main ()
+{
+   	int a;
+	int b;
+
+	a = 5;
+	b = ft_iterative_factorial(a);
+	printf("%d\n", b);
+	return (0);
+}
---
+++ Day04/ft_recursive_factorial.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbourhis <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/03/12 17:17:33 by nbourhis          #+#    #+#             */
+/*   Updated: 2017/03/12 19:19:24 by nbourhis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+int		ft_iterative_factorial(int nb)
+{
+	if (nb >  1)
+	{
+		return (nb * ft_iterative_factorial(nb - 1));
+	}
+	else
+	{
+		return (1);
+	}
+
+}
+
+int		 main()
+{
+	int a;
+	int b;
+
+	a = 5;
+	b = ft_iterative_factorial(a);
+	printf("%d\n", b);	
+}
